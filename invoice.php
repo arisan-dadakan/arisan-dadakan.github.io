858713bff5790f6244c7d279462976d5ec74d675
arisan-dadakan <86888799+arisan-dadakan@users.noreply.github.com>
2021-07-04 01:25:39 +0700

    Create invoice.php

---
+++ invoice.php
@@ -0,0 +1,187 @@
+
+
+
+      
+      
+      
+                    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
+    
+    
+    
+    <!DOCTYPE html>
+<html lang="en" >
+<head>
+  <meta charset="UTF-8">
+ <meta name='viewport' content='width=device-width, initial-scale=1'>
+ <title>Informasi Pembayaran Arisan</title>
+  <link rel="stylesheet" href="./style.css">
+
+</head>
+<body>
+<!-- partial:index.partial.html -->
+<link rel="preconnect" href="https://fonts.gstatic.com">
+<link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+<script src="https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js" type="text/javascript"></script>
+<script src="jq/jquery.countdown.min.js"></script>
+<script src="jq/jquery.countdown.js"></script>
+
+
+  <div class="header-inv"><div class="news blue">
+<span class="text1" ><marquee><b>INFORMASI PEMBAYARAN ARISAN</b> &nbsp;&nbsp; &nbsp; &nbsp; Bagi Anggota Grup yang belum melakukan Transfer, Mohon segera Transfer ke Rekening HENDRI SILAEN sebesar :
+    Rp.1.000.000 sebelum tanggal 06 July 2021.</marquee></span>
+</div><br>
+   Pemenang Arisan Bulan ini
+  
+  <div class="body-inv">
+    <div class="account">
+    <h2>HENDRI SILAEN</h2>
+    No.Rek <b>Cimb Niaga</b>:<h2>  7064 9311 6700</h2>
+        
+    </div></div>
+    
+    <p>Bagi Anggota Grup yang belum melakukan Transfer, Mohon segera Transfer ke Rekening HENDRI SILAEN sebesar :</p>
+    <h3 class="nominal" >Rp.1.000.000</h3>
+    
+    <div class="attention">
+      <h4><span style="color:red">PENTING</span> : Setelah Anda melakukan transfer, Mohon segera Upload bukti pembayaran ke Grup WhatsApp Arisan_Dadakan!.</h4>
+    </div>
+    <br>
+    Menunggu Pembayaran
+    <lottie-player src="https://assets1.lottiefiles.com/datafiles/kn5W819UTw4eDwEBTOscVxDtsBaRzRSLnlqWen3o/Loading/data.json"  background="transparent"  speed="2"  style="width: 100%; height: 150px; margin-top:-50px"  loop  autoplay></lottie-player>
+     <div class="detail">
+       <center>
+      <img src="cimb.png" width="140px" style="text-align:center !important"><br>
+       </center><br>
+       <table>
+          <tr>
+            <td>
+                Nama Bank 
+            </td>
+            <td>
+            :
+            </td>
+            <td><b>Cimb Niaga</b> </td>
+         </tr>
+         <tr>
+            <td>
+                No. Rekening 
+            </td>
+            <td>
+            :
+            </td>
+           <td><b><input type="text"  value="7064 9311 6700" id="rekening" style="visibility:show; border:none; background:#eee;" readonly ></b></td>   <td><button class="salin" id="salin2" onClick="salinRekening()">Salin</button></td>
+                      
+         </tr>
+         
+         
+         
+         
+         
+         <tr>
+            <td>
+                Nama Penerima 
+            </td>
+            
+            <td>
+            :
+            </td>
+            <td><b>HENDRI SILAEN</b></td>
+         </tr>
+         <tr>
+            <td>Total Pembayaran
+            </td>
+            <td>
+            :
+            </td>
+            <td><b>Rp.1.000.000</b></td>
+         </tr>
+         <tr>
+            <td>
+                Nama Program 
+            </td>
+            <td>
+            :
+            </td>
+            <td cols="2">Pembayaran Arisan Bulan Juli</td>
+         </tr>
+       </table>
+       
+       <p style="color:deepskyblue"> Batas pembayaran  06 July 2021 <br><span id="clock"> </span></p>
+    </div><br><br>
+  </div>
+  
+</div>
+<!-- partial -->
+ <script>									
+					$('#clock').countdown('2021/07/06 23:00:00', function(event) {
+					  $(this).html(event.strftime('%D hari lagi, %Hjam %Mmenit %Sdetik'));
+					});	
+					</script>
+  <script  src="./script.js"></script>
+</body>
+
+<hr>
+<footer style="padding : 10px; font-size: 12px; text-align : center; ">&copy; 2021 Arisan_Dadakan PT.nittoh Batam</footer>
+</html>
+
+    
+   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+   
+
+
+
+
+
